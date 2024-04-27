081fccfd6b0468d2b1591d5110473151fb0efbc2
Hector Palencia <hrpmicarelli@gmail.com>
2024-04-27 10:54:59 -0400

    act timer

--- plugins/functions.js
+++ plugins/functions.js
@@ -4,7 +4,6 @@ export function formattedTime(seconds) {
   let hour = Math.floor(seconds / 3600);
   hour = (hour < 10)? '0' + hour : hour;
   let minute = Math.floor((seconds / 60) % 60);
-  console.log('minute: ', minute)
   minute = (minute < 10)? '0' + minute : minute;
   let second = seconds % 60;
   second = (second < 10)? '0' + second : second;
