601c3039c7aea18ecadbf8ba6ccd2fff8b11fd05
dvconsultores <adominguez@dvconsultores.com>
2024-06-15 12:05:09 -0400

    refactor: Update withdraw query to include remaining amount filter

--- pages/withdraw.vue
+++ pages/withdraw.vue
@@ -338,7 +338,7 @@ export default {
       const selects = gql`
       query MyQuery( $token: String, $address: String ) {
         offerssells(
-          where: {asset_contains: $token, owner_id_not: $address, is_pause: false}
+          where: {asset_contains: $token, owner_id_not: $address, is_pause: false, remaining_amount_gte: "1"}
           orderBy: exchange_rate
           orderDirection: desc
         ) {
